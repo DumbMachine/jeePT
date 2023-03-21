1f71e4feba1922e8e0363ec184eb5962a21b1950
DumbMachine <ratin.kumar.2k@gmail.com>
2023-03-21 14:40:01 +0530

    meh commit

--- data.py
+++ data.py
@@ -1,90 +1,299 @@
 questions = [
     {
         "text": """The equation of wave is given as y = 0.05 sin(2𝑥 − 4𝑡), where 𝑥 in meters and 𝑡 is time in seconds. The velocity of the wave is equal to""",
-
-         "option": ["""2""",
-         """4""",
-         """0.5""",
-         """0.25"""],
+        "options": ["""2""", """4""", """0.5""", """0.25"""],
         "answer": "A",
     },
     {
         "text": """Two charges 𝑞1 and 𝑞2 separated by a distance 𝑑 are placed in a medium of dielectric constant 𝑘, if they are placed in the air then find equivalent distance at which they experience same force.""",
-         "option": ["""𝑑√𝑘""",
-         """𝑘√𝑑""",
-         """2𝑑√𝑘""",
-         """1.5𝑑√𝑘"""],
+        "options": ["""𝑑√𝑘""", """𝑘√𝑑""", """2𝑑√𝑘""", """1.5𝑑√𝑘"""],
         "answer": "A",
     },
     {
         "text": """In the circuit shown, Find the current through 𝑅4(𝐼4) and 𝑅5(I5)""",
-         "option": ["""I4=(24/55)A,I5=(96/55)A""",
-         """I4=(96/55)A,I5=(24/55)A""",
-         """I4=(24/37)A,I5=(96/37)A""",
-         """I4=(96/37)A,I5=(24/37)A"""],
+        "options": [
+            """I4=(24/55)A,I5=(96/55)A""",
+            """I4=(96/55)A,I5=(24/55)A""",
+            """I4=(24/37)A,I5=(96/37)A""",
+            """I4=(96/37)A,I5=(24/37)A""",
+        ],
         "answer": "A",
     },
     {
         "text": """A circular loop of radius 10 𝑐𝑚 is placed in a linearly varying perpendicular magnetic field which has magnitude √𝜋 0.5 𝑇 at time 𝑡 = 0. The magnetic field reduces to zero at 𝑡 = 0.5 𝑠𝑒𝑐. Find the 𝑒𝑚𝑓 induced in the loop at 𝑡 = 0.25 𝑠𝑒𝑐.""",
-         "option": ["""0.01 𝑉""",
-         """0.005 𝑉""",
-         """0.02 𝑉""",
-         """0.03 𝑉"""],
+        "options": ["""0.01 𝑉""", """0.005 𝑉""", """0.02 𝑉""", """0.03 𝑉"""],
         "answer": "A",
     },
     {
         "text": """If a ball is thrown from ground in vertical plane, it attains maximum height of 360 𝑚. Find the maximum distance, the ball can cover on ground keeping the projection speed constant.""",
-         "option": ["""360𝑚""",
-         """720𝑚""",
-         """1440 𝑚""",
-         """180𝑚"""],
+        "options": ["""360𝑚""", """720𝑚""", """1440 𝑚""", """180𝑚"""],
         "answer": "B",
     },
     {
         "text": """Which statement is correct about photoelectric effect?""",
-         "option": ["""Maximum kinetic energy depends upon intensity of light.""",
-         """Stopping potential is dependant only on work function of metal.""",
-         """Photoelectric effect can be explained by wave nature of light.""",
-         """Photoelectric effect can be explained by particle nature of light."""],
+        "options": [
+            """Maximum kinetic energy depends upon intensity of light.""",
+            """Stopping potential is dependant only on work function of metal.""",
+            """Photoelectric effect can be explained by wave nature of light.""",
+            """Photoelectric effect can be explained by particle nature of light.""",
+        ],
         "answer": "D",
     },
     {
         "text": """Two parallel infinite wires carry equal currents are arranged parallel to each other with same direction of current. If both the currents are doubled and separation is halved, the force on a 10 𝑐𝑚 section of one of the wires becomes:""",
-         "option": ["""4 times""",
-         """1/4 times""",
-         """8 times""",
-         """1/8 times"""],
+        "options": ["""4 times""", """1/4 times""", """8 times""", """1/8 times"""],
         "answer": "C",
     },
     {
         "text": """Statement 1: Photodiodes are operated in reverse biased. Statement 2 : Current in forward biased is more than current in reverse bias in 𝑝 − 𝑛 diode.""",
-         "option": ["""Both the statements are true.""",
-         """Statement 1 is true and statement 2 is false.""",
-         """Statement 1 is true and statement 2 is false.""",
-         """Both the statements are false."""],
+        "options": [
+            """Both the statements are true.""",
+            """Statement 1 is true and statement 2 is false.""",
+            """Statement 1 is true and statement 2 is false.""",
+            """Both the statements are false.""",
+        ],
         "answer": "A",
     },
     {
         "text": """Weight of an object at earth’s surface is 18 𝑁. If the object is taken 3200 𝑘𝑚 above the surface, then the weight of the object (in 𝑁) is_______ .(Given; radius of Earth =6400 𝑘𝑚)""",
-        "option": [],
+        "options": [],
         "answer": "8",
     },
     {
         "text": """Statement 1: If the weight of the lift is equal to the tension force of the cable wire, then it moves with uniform velocity. Statement 2: If the lift moves downward with an acceleration, then the contact force between the boy’s feet and lift floor is more than the weight of boy.""",
-         "option": ["""Both the statements are true and (2) is the correct explanation of (1)""",
-         """Both the statements are true and (2) is not the correct explanation of (1)""",
-         """Statement 1 is true and statement 2 is false.""",
-         """Statement 2 is true and statement 1 is false."""],
+        "options": [
+            """Both the statements are true and (2) is the correct explanation of (1)""",
+            """Both the statements are true and (2) is not the correct explanation of (1)""",
+            """Statement 1 is true and statement 2 is false.""",
+            """Statement 2 is true and statement 1 is false.""",
+        ],
         "answer": "C",
+    },
+    {
+        "text": """
+Let 𝛾1 be the ratio of molar specific heat at constant pressure and constant volume for a monoatomic gas and let 𝛾2 be this ratio for a diatomic gas. Find 𝛾1
+    """.strip(),
+        "options": [
+            "21/25",
+            "7/3",
+            "25/21",
+            "3/7",
+        ],
+        "answer": "C",
+    },
+    {
+        "text": """
+Assertion (𝐴): Steel is used to build big structures.
+Reason (𝑅): Steel has more elastic modulus as compared to other materials.
+    """.strip(),
+        "options": [
+            "Both 𝐴 and 𝑅 are true and 𝑅 is the correct explanation of 𝐴",
+            "Both 𝐴 and 𝑅 are true but 𝑅 is not the correct explanation of 𝐴",
+            "𝐴 is true but 𝑅 is false",
+            "Both 𝐴 and 𝑅 are false",
+        ],
+        "answer": "A",
+    },
+    {
+        "text": """
+If 𝑎⃗ = 𝑖̂ + 2𝑗̂ + 𝑚𝑘 and 𝑏 = 𝑖̂ − 2𝑗̂ + 𝑚𝑘 are perpendicular to each other, then 𝑚 = √𝑥. Find 𝑥.
+    """.strip(),
+        "options": [],
+        "answer": "3",
+    },
+    {
+        "text": """
+The distance of earth from the sun is 1.5 × 108 𝑘𝑚. If the time period of an imaginary planet is 2.83 𝑦𝑒𝑎𝑟𝑠, then its distance from the sun comes out to be 𝑛 × 108 𝑘𝑚. Find 𝑛.
+    """.strip(),
+        "options": [],
+        "answer": "3",
+    },
+    {
+        "text": """
+An electron jumps from 3𝑟𝑑 excited state to the ground state in a hydrogen atom. The wavelength of photon emitted in 𝑛𝑚 to the nearest integer is _____ (h𝑐 = 1240 𝑒𝑉 − 𝑛𝑚)
+    """.strip(),
+        "options": [],
+        "answer": "97",
+    },
+    {
+        "text": """
+Assertion (𝐀): Acceleration due to gravity decreases with both height and depth from earth surface. Reason (𝐑): If height and depth are equal for two points from surface of the earth, then acceleration due to
+gravity will be same at those points.
+    """.strip(),
+        "options": [
+            "Both 𝐴 and 𝑅 are true and 𝑅 is the correct explanation of 𝐴",
+            "Both 𝐴 and 𝑅 are true and 𝑅 is not the correct explanation of 𝐴",
+            "𝐴 is true but 𝑅 is false.",
+            "Both 𝐴 and 𝑅 are false",
+        ],
+        "answer": "C",
+    },
+    {
+        "text": """
+⃗Electric field vector and magnetic field vector of an electromagnetic wave is given as 𝐸 = 𝐸0 sin(𝜔𝑡 − 𝑘𝑥)𝑗̂
+⃗⃗ ̂ and 𝐵 = 𝐵0 sin(𝜔𝑡 − 𝑘𝑥)𝑘, then choose the correct option.
+    """.strip(),
+        "options": [
+            "𝜔𝐸0=𝑘𝐵0",
+            "𝑘𝐸0=𝜔𝐵0",
+            "𝑘𝜔=𝐸0𝐵0",
+            "None of these",
+        ],
+        "answer": "B",
+    },
+    {
+        "text": """
+A long solenoid has 70 turns per 𝑐𝑚 and carries current 2 𝐴. The magnetic field inside the solenoid is____ (𝜇𝑜 = 4𝜋 × 10−7 𝑖𝑛 𝑆𝐼 𝑢𝑛𝑖𝑡𝑠)
+    """.strip(),
+        "options": [
+            "125.2 × 10−4 𝑇",
+            "835.2 × 10−4 𝑇",
+            "176.0 × 10−4 𝑇",
+            "880×10−4𝑇",
+        ],
+        "answer": "C",
+    },
+    {
+        "text": """
+ A ray of parallel white light incidence on convex lens and colour gets separated, this phenomenon is called
+    """.strip(),
+        "options": [
+            "Chromatic aberration",
+            "Polarization",
+            "Scattering",
+            "Spherical aberration",
+        ],
+        "answer": "A",
+    },
+    {
+        "text": """
+Assertion (𝐀): The weight of an object at Mount Everest is lesser than its weight at sea level. Reason (𝐑): The value of 𝑔 decreases as height increases.
+    """.strip(),
+        "options": [
+            "Assertion and Reason both are correct, and Reason is the correct explanation of Assertion",
+            "Assertion and Reason both are correct, but Reason is not the correct explanation",
+            "Assertion is correct reason is incorrect",
+            "Assertion is incorrect reason is correct",
+        ],
+        "answer": "A",
+    },
+    {
+        "text": """
+A convex lens of refractive index 1.5 has focal length 20 𝑐𝑚 in air. When this lens is immersed in water (refractive index = 4/3), the new focal length (𝑖𝑛 𝑐𝑚) is __________.
+    """.strip(),
+        "options": [],
+        "answer": "80",
+    },
+    {
+        "text": """
+Radius of 2nd orbit of Li2+ ion is x, radius of 3rd orbit of Be3+ will be
+3. Thionyl chloride on reaction with white phosphorous gives compound A. A on hydrolysis gives compound B which is dibasic. Identify A and B.
+    """.strip(),
+        "options": [
+            "27x/16",
+            "(3/4)x",
+            "16x/27",
+            "(4/3)x",
+        ],
+        "answer": "A",
+    },
+    {
+        "text": """
+If X-atoms are present at alternate corners and at body centre of a cube and Y-atoms are present at 1/3rd of face centres then what will be empirical formula?
+    """.strip(),
+        "options": [
+            "X2·5Y",
+            "X5Y2",
+            "X1·5Y2",
+            "X3Y2",
+        ],
+        "answer": "D",
+    },
+    {
+        "text": """
+The correct decreasing order of positive electron gain enthalpy for the following inert gases
+He, Ne, Kr, Xe
+    """.strip(),
+        "options": [
+            "He>Ne>Kr>Xe",
+            "He>Ne>Xe>Kr",
+            "He>Xe>Ne>Kr",
+            "Ne>Kr>Xe>He",
+        ],
+        "answer": "D",
+    },
+    {
+        "text": """
+Which of the following is correct about antibiotics.
+    """.strip(),
+        "options": [
+            "Antibioticsarethesubstancesthatpromotethe growth of microorganism",
+            "Penicillin has bacteriostatic effect",
+            "Erythromycin has Bactericidal effect",
+            "These are synthesized artificially",
+        ],
+        "answer": "D",
+    },
+    {
+        "text": """
+A carnot engine working between a source and sink at 200 K has efficiency of 50%. Another carnot engine working between the same source and another sink with unknown temperature T has efficiency of 75%. The value of T is equal to
+    """.strip(),
+        "options": [
+            "400 K",
+            "300 K",
+            "100 K",
+            "200 K",
+        ],
+        "answer": "D",
+    },
+    {
+        "text": """
+Mark the option correctly matching the following columns with appropriate dimensions
+Column-I                Column-II
+(A) Surface tension     (P) [ML^–1T^–2]
+(B) Pressure            (Q) [MT^–2]
+(C) Viscosity           (R) [MLT^–1]
+(D) Impulse             (S) ML^–1T^–1
+    """.strip(),
+        "options": [
+            "A(Q), B(P), C(R), D(S)",
+            "A(Q), B(P), C(S), D(R)",
+            "A(S), B(Q), C(P), D(R)",
+            "A(R), B(P), C(Q), D(S)",
+        ],
+        "answer": "B",
+    },
+    {
+        "text": """
+Temperature of hot soup in a bowl goes from 98°C to 86°C in 2 minutes. The temperature of surroundings is 22°C. Find the time taken for the temperature of soup to go from 75°C to 69°C. [Assume Newton’s law of cooling is valid]
+    """.strip(),
+        "options": [
+            "1 minute",
+            "1.4 minute",
+            "2 minute",
+            "3.2 minute",
+        ],
+        "answer": "B",
+    },
+    {
+        "text": """
+Electric field is applied along +y direction. A charged
+particle is travelling along −kˆ, undeflected. Then magnetic field in the region will be along?
+        """.strip(),
+        "options": [
+            "iˆ",
+            "−iˆ",
+            "ˆj",
+            "−kˆ",
+        ],
+        "answer": "A",
+    },
+    {
+        "text": """
+A solenoid of length 2 m, has 1200 turns. The magnetic field inside the solenoid when 2 A current is passed through it is Nπ×10−5 T.Find the value of N. (Diameter of solenoid is 0.5 m)
+        """.strip(),
+        "options": [],
+        "answer": "48",
     },
 ]
 
-alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-formatOption= lambda opt, idx: f'Option({alphabet[idx]}): {opt}'
-
-for ques in questions:
-    options = "\n".join([
-        formatOption(opt, idx) for idx, opt in enumerate(ques["option"])
-    ])
-
-    prompt_ = f"""{ques['text']}\n{options}\nAnswer:{ques["answer"]}""".format(question=ques["text"])
